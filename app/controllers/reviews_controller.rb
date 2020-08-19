0c009342aab39c678abdb40392d26a2d7ea411b0
gabriel belo <gabrielrbelo@gmail.com>
2020-08-19 16:08:42 -0300

    rails yelp done with navbar

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,11 @@
 class ReviewsController < ApplicationController
-  def new
-    @review = Review.new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
-
   def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render :new
-    end
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
